e3a11f55540bb561ebe92837136a9a224198ae02
Mário Menezes <mario082@gmail.com>
2021-02-01 15:18:48 -0300

    background image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import styled from 'styled-components'
+import db from '../db.json'
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.success};
 `
 
-export default function Home() {
-  return <Title>Cafeína Quiz</Title>
+const backgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+
+`
+
+export default function Home(){
+  return(
+    <div style={{ backgroundImage: `url(${db.bg})` }}>
+      teste
+    </div>
+  );
 }
+
